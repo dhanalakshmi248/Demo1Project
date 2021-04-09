028f6e38165b6cedb9a2705696ef867d33743da9
dhana <dhana@DESKTOP-K0EB7LP>
2021-04-09 11:48:25 +0530

    added TestGitPrgrm class

---
+++ Demo1Project/src/com/git/com/TestGitPrgrm.java
@@ -0,0 +1,12 @@
+package com.git.com;
+
+public class TestGitPrgrm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=10;
+int j=30;
+System.out.println(i+"   "+j);
+	}
+
+}
